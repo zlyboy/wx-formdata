4efecfd7179bd1d1d63f902c76565c510043f152
zlyboy <zlyboy@126.com>
2020-04-26 14:38:47 +0800

    改utf8编码

--- formData.js
+++ formData.js
@@ -53,7 +53,7 @@ function convert(data, files){
   //结尾
   let endBoundaryArray = [];
   for (var i = 0; i < endBoundary.length; i++) { // 最后取出结束boundary的charCode
-    endBoundaryArray.push(endBoundary.charCodeAt(i));
+    endBoundaryArray.push(...endBoundary.utf8CodeAt(i));
   }
   postArray = postArray.concat(endBoundaryArray);
   return {
@@ -96,15 +96,15 @@ function formDataArray(boundary, name, value, fileName){
 
   var dataArray = [];
   for (var i = 0; i < dataString.length; i++) { // 取出文本的charCode（10进制）
-    dataArray.push(dataString.charCodeAt(i));
+    dataArray.push(...dataString.utf8CodeAt(i));
   }
 
   if (isFile) {
     let fileArray = new Uint8Array(value);
     dataArray = dataArray.concat(Array.prototype.slice.call(fileArray));
   }
-  dataArray.push("\r".charCodeAt());
-  dataArray.push("\n".charCodeAt());
+  dataArray.push(..."\r".utf8CodeAt());
+  dataArray.push(..."\n".utf8CodeAt());
 
   return dataArray;
 }
@@ -115,6 +115,32 @@ function getFileMime(fileName){
   return mime?mime:"application/octet-stream"
 }
 
-module.exports = FormData;
+String.prototype.utf8CodeAt = function(i) {
+  var str = this;
+  var out = [], p = 0;
+  var c = str.charCodeAt(i);
+  if (c < 128) {
+    out[p++] = c;
+  } else if (c < 2048) {
+    out[p++] = (c >> 6) | 192;
+    out[p++] = (c & 63) | 128;
+  } else if (
+      ((c & 0xFC00) == 0xD800) && (i + 1) < str.length &&
+      ((str.charCodeAt(i + 1) & 0xFC00) == 0xDC00)) {
+    // Surrogate Pair
+    c = 0x10000 + ((c & 0x03FF) << 10) + (str.charCodeAt(++i) & 0x03FF);
+    out[p++] = (c >> 18) | 240;
+    out[p++] = ((c >> 12) & 63) | 128;
+    out[p++] = ((c >> 6) & 63) | 128;
+    out[p++] = (c & 63) | 128;
+  } else {
+    out[p++] = (c >> 12) | 224;
+    out[p++] = ((c >> 6) & 63) | 128;
+    out[p++] = (c & 63) | 128;
+  }
+  return out;
+};
 
+
+module.exports = FormData;
 
