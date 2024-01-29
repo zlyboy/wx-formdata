9308eb76e179e2f99cade46c4cf2624680c9fa58
lmj01 <lmj01@users.noreply.github.com>
2024-01-29 08:38:23 +0800

    Update formData.js
    TextEncoder不稳定，偶尔可用，至少微信上文档没有说明这个问题

--- formData.js
+++ formData.js
@@ -1,6 +1,41 @@
 const mimeMap = require('./mimeMap.js')
 
-const coder = new TextEncoder();
+// const coder = new TextEncoder();
+const coder = function() {
+  const encode = (str)=>{
+      const bytes = [];
+      for (let i = 0; i < str.length; i++) {
+          let out = [], p = 0;
+          const c = str.charCodeAt(i);
+          if (c < 128) {
+              out[p++] = c;
+          } else if (c < 2048) {
+              out[p++] = (c >> 6) | 192;
+              out[p++] = (c & 63) | 128;
+          } else if (
+              ((c & 0xFC00) == 0xD800) && (i + 1) < str.length &&
+              ((str.charCodeAt(i + 1) & 0xFC00) == 0xDC00)) {
+              // Surrogate Pair
+              c = 0x10000 + ((c & 0x03FF) << 10) + (str.charCodeAt(++i) & 0x03FF);
+              out[p++] = (c >> 18) | 240;
+              out[p++] = ((c >> 12) & 63) | 128;
+              out[p++] = ((c >> 6) & 63) | 128;
+              out[p++] = (c & 63) | 128;
+          } else {
+              out[p++] = (c >> 12) | 224;
+              out[p++] = ((c >> 6) & 63) | 128;
+              out[p++] = (c & 63) | 128;
+          }
+          bytes.push(...out);
+          if (c > 0xffff) i++;
+      }
+      return bytes;
+  }
+  return {
+      encode,
+  }
+}();
+
 
 function FormData(){
   let fileManager = wx.getFileSystemManager();
